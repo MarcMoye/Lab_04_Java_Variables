1d700427c27f158ee1fb75db025aea2abcbb55e5
marcmoye <moyemarc24@gmail.com>
2025-09-18 15:53:08 -0400

    Completed ProjectT

--- src/Main.java
+++ src/Main.java
@@ -18,19 +18,41 @@ public class Main {
         int intModulo = intOperandA % intOperandB;
         System.out.println("The modulo of " + intOperandA + " and " + intOperandB + " is " + intModulo);
 
-        double doubleOperandA = (double)5.5F;
-        double doubleOperandB = (double)15.5F;
+        double doubleOperandA = 5.5;
+        double doubleOperandB = 15.5;
 
         double doubleSum = doubleOperandA + doubleOperandB;
-        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " = " + doubleSum);
 
         double doubleProduct = doubleOperandA * doubleOperandB;
-        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " = " + doubleProduct);
 
         double doubleDifference = doubleOperandB - doubleOperandA;
-        System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+        System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " = " + doubleDifference);
 
         double doubleQuotient = doubleOperandB / doubleOperandA;
         System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+        int intKids = 3;
+        System.out.println("The number of kids in my family is " + intKids);
+
+        boolean IsItRaining = false;
+        System.out.println("Is it raining? " + "false");
+
+        int favNumber = 30;
+        System.out.println("My favorite number is " + favNumber);
+
+        double shoesSize = 11.5;
+        System.out.println("My shoe size is " + shoesSize);
+
+        int birthMonth = 1; // 1= January, 2= February, etc.
+        String[] monthName = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        System.out.println("I was born in the month of "  + monthName [birthMonth - 1]);
+
+        String fullName = "Marc Moye";
+        System.out.println("My full name is " + fullName);
+
+
+
     }
 }
